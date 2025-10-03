5e09a44164ce879607388199bf201d63e15bb7bc
TutajITeraz <boldalukasz@gmail.com>
2025-10-03 14:49:00 +0200

    Auto packaging releases

---
+++ ritus-server/package.sh
@@ -0,0 +1,16 @@
+zip -r ../ritus_plus.zip \
+  ai_tools.py \
+  batch_analysis.py \
+  cache_config.py \
+  easy_install_mac.sh \
+  easy_install.sh \
+  image_processing.py \
+  krakenServer.py \
+  models.py \
+  package-lock.json \
+  package.json \
+  README.txt \
+  run_server_without_kraken.sh \
+  run_server.sh \
+  server.py \
+  static
