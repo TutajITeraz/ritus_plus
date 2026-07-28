f84a15707deac56e69fc613626960eead065a46a
TutajITeraz <boldalukasz@gmail.com>
2026-07-28 15:10:56 +0200

    X for closing all toasts

--- ritus-client/src/components/ui/toaster.jsx
+++ ritus-client/src/components/ui/toaster.jsx
@@ -1,24 +1,3 @@
-function _optionalChain(ops) {
-  let lastAccessLHS = undefined
-  let value = ops[0]
-  let i = 1
-  while (i < ops.length) {
-    const op = ops[i]
-    const fn = ops[i + 1]
-    i += 2
-    if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) {
-      return undefined
-    }
-    if (op === 'access' || op === 'optionalAccess') {
-      lastAccessLHS = value
-      value = fn(value)
-    } else if (op === 'call' || op === 'optionalCall') {
-      value = fn((...args) => value.call(lastAccessLHS, ...args))
-      lastAccessLHS = undefined
-    }
-  }
-  return value
-}
 ;('use client')
 
 import {
@@ -55,13 +34,7 @@ export const Toaster = () => {
             {toast.action && (
               <Toast.ActionTrigger>{toast.action.label}</Toast.ActionTrigger>
             )}
-            {_optionalChain([
-              toast,
-              'access',
-              (_) => _.meta,
-              'optionalAccess',
-              (_2) => _2.closable,
-            ]) && <Toast.CloseTrigger />}
+            <Toast.CloseTrigger />
           </Toast.Root>
         )}
       </ChakraToaster>
